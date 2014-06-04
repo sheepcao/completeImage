63ab23583f342cb965404fbf259090e8528afc81
sheepcao <sheepcao1986@163.com>
2014-06-04 18:01:34 +0800

    Initial Commit

---
+++ completeImage/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  completeImage
+//
+//  Created by 张力 on 14-6-4.
+//  Copyright (c) 2014年 张力. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ completeImage/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  completeImage
+//
+//  Created by 张力 on 14-6-4.
+//  Copyright (c) 2014年 张力. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
